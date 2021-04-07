3ed87644c632a701ed5a1cca114710c9a9153112
vansinh <tvsinh.007@gmail.com>
2021-04-07 23:28:56 +0700

    update backend

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,26 @@
+import express from 'express'
+const router = express.Router()
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+  getRelatedProducts
+} from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
+router.route('/:id/recommend')
+  .get(getRelatedProducts)
+
+export default router
